d899f20dc2c1224277faab173929b757953f93b7
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2021-10-08 19:08:03 -0700

    lecture 6

--- R/main.R
+++ R/main.R
@@ -1,5 +1,4 @@
-install.packages("datavolley", repos = "https://openvolley.r-universe.dev")
 library(datavolley)
 
-x <- dv_read(dv_example_file(), insert_technical_timeouts = FALSE)
+x <- dv_read("../scout-files/test.dvw", insert_technical_timeouts = FALSE)
 summary(x)
