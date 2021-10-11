fc16a7050ce5775c268834316a9bf93273729e22
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2021-10-11 00:19:41 -0700

    added comments to file

--- R/main.R
+++ R/main.R
@@ -13,14 +13,13 @@ lx <- lapply(d, dv_read, insert_technical_timeouts = FALSE)
 px <- list()
 px <- do.call(rbind, lapply(lx, plays))
 
+# adds the reception rating
 rq <- px %>% dplyr::filter(skill == "Reception") %>% group_by(match_id, point_id) %>%
   dplyr::summarize(reception_quality = if (n() == 1) .data$evaluation else NA_character_) %>% ungroup
 
 px <- px %>% left_join(rq, by = c("match_id", "point_id"))
 
-# attacking <- px %>% dplyr::filter(skill == "Attack" & phase == "Reception" & grepl("Perfect|Positive", reception_quality)) %>%
-#  group_by(player_name) %>% dplyr::summarize(kill_rate = mean(evaluation == "Winning attack"))
-
+# calculates attacking percentages
 attacking <- px %>% dplyr::filter(skill == "Attack") %>%
  group_by(player_name) %>% dplyr::summarize(kill_pct = mean(evaluation == "Winning attack"))
 
@@ -33,15 +32,19 @@ blocked <- px %>% dplyr::filter(skill == "Attack") %>%
 eff <- px %>% dplyr::filter(skill == "Attack") %>%
   group_by(player_name) %>% dplyr::summarize(eff_pct = mean((evaluation == "Winning attack") - (evaluation == "Error") - (evaluation == "Blocked")))
 
+# joins the tables together
 attacking <- attacking %>% left_join(error, by = c("player_name"))
 attacking <- attacking %>% left_join(blocked, by = c("player_name"))
 attacking <- attacking %>% left_join(eff, by = c("player_name"))
 
+# write a .csv file for the table
 write.csv(attacking, file = "test.csv", append = FALSE, quote = TRUE, sep = " ",
             eol = "\n", na = "NA", dec = ".", row.names = TRUE,
             col.names = TRUE, qmethod = c("escape", "double"),
             fileEncoding = "")
 
+# create and a sql database the .csv file
 test_db <- rio::import("test.csv")
 
+# outputs the database as an excel file
 output_db <- export(test_db, "spredsheet.xlsx")
